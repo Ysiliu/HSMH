f46386e8327961e16b7f11df6c1876939924d685
Ysiliu <119477455+Ysiliu@users.noreply.github.com>
2024-08-23 16:23:21 +0800

    new

--- data/generate_data.py
+++ data/generate_data.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 # *-* coding:utf8 *-*
 
-"""
-作者: sjjin
-邮件: sj_jin@vip.hnist.edu.cn
-日期: 2022年12月20日
-说明: 生成流
-"""
-
 from component.flow import Flow,FlowType
 from enum import Enum
 import random
